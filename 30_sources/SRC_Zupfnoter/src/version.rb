2734b4ec84fbfdb65ea8e0ad16e5ffd8e35e65b0
Bernhard Weichel <github.com@nospam.weichel21.de>
2016-07-14 21:16:45 +0200

    Adapted version string for beta

--- 30_sources/SRC_Zupfnoter/src/version.rb
+++ 30_sources/SRC_Zupfnoter/src/version.rb
@@ -1,3 +1,3 @@
-VERSION = "1.3.2 alpha"
+VERSION = "1.3.2 beta"
 SCHEMA_VERSION = 'https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json'
-COPYRIGHT = "©#{Time.now.year} http://www.bernhard-weichel.de/zupfnoterpage"
+COPYRIGHT = "©#{Time.now.year} https://www.zupfnoter.de.de"
