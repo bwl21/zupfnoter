f12699ae0d4ebf3a27daea0c22b40a8beb9068da
Bernhard Weichel <github.com@nospam.weichel21.de>
2016-07-31 20:50:54 +0200

    fixed url

--- 30_sources/SRC_Zupfnoter/src/version.rb
+++ 30_sources/SRC_Zupfnoter/src/version.rb
@@ -1,3 +1,3 @@
 VERSION = "1.3.2 beta 2"
 SCHEMA_VERSION = 'https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json'
-COPYRIGHT = "©#{Time.now.year} https://www.zupfnoter.de.de"
+COPYRIGHT = "©#{Time.now.year} https://www.zupfnoter.de"
