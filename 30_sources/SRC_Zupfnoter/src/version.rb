1a42501449806eabc2f6bc2d296e49566199f89d
Bernhard Weichel <github.com@nospam.weichel21.de>
2016-08-23 13:40:11 +0200

    bumped development version

--- 30_sources/SRC_Zupfnoter/src/version.rb
+++ 30_sources/SRC_Zupfnoter/src/version.rb
@@ -1,3 +1,3 @@
-VERSION = "1.4.0"
+VERSION = "1.5.0 dev"
 SCHEMA_VERSION = 'https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json'
 COPYRIGHT = "©#{Time.now.year} https://www.zupfnoter.de"
