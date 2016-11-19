cb2ecd827a4d6a21270f346722a9080aca9ed289
Bernhard Weichel <github.com@nospam.weichel21.de>
2016-11-19 12:02:17 +0100

    bumped version to be 1.5.0 beta 1

--- 30_sources/SRC_Zupfnoter/src/version.rb
+++ 30_sources/SRC_Zupfnoter/src/version.rb
@@ -1,4 +1,4 @@
-VERSION = "1.5.0 dev 5"
+VERSION = "1.5.0 beta 1"
 SCHEMA_VERSION = 'https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json'
 COPYRIGHT = "©#{Time.now.year} https://www.zupfnoter.de"
 LOCATION = Controller::get_uri[:hostname]
