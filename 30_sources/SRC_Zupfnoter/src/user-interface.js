bwl21/zupfnoter

commit 85fbce845f05ceec92cc50a59349f17d51023423

#135 block write-orations in demo mode

30_sources/SRC_Zupfnoter/src/user-interface.js
@@ -171,7 +171,7 @@ function init_w2ui(uicontroller) {
           "Ok": function () {
             if (this.validate().length == 0) {
               w2popup.close();
-              uicontroller.$handle_command("dlogin full \"" + this.record.folder+ "\"")
+              uicontroller.$handle_command("dlogin full \"" + this.record.folder + "\"")
             }
           },
           "Cancel": function () {
@@ -640,6 +640,11 @@ function init_w2ui(uicontroller) {
         id: 'sb_loglevel',
         text: '<div style="padding: 0px !important;"><span class="sb-loglevel" "></span></div>'
       },
+      {
+        type: 'button',
+        id: 'sb_mode',
+        text: '<div style="padding: 0px !important;"><span class="sb-mode" "></span></div>'
+      },
       {type: 'spacer'},
       {
         type: 'button',
@@ -806,6 +811,24 @@ function update_systemstatus_w2ui(systemstatus) {
   ;
 
   $(".sb-loglevel").html('Loglevel: ' + systemstatus.loglevel);
+  $(".sb-mode").html(w2utils.lang('Mode') + ': ' + systemstatus.mode);
+
+  if (systemstatus.mode == 'demo') {
+    w2ui.layout_top_toolbar.disable('tb_create')
+    w2ui.layout_top_toolbar.disable('tb_open')
+    w2ui.layout_top_toolbar.disable('tb_save')
+    w2ui.layout_top_toolbar.disable('tbDropbox')
+    w2ui.layout_top_toolbar.disable('tb_login')
+  }
+  else {
+    w2ui.layout_top_toolbar.enable('tb_create')
+    w2ui.layout_top_toolbar.enable('tb_open')
+    w2ui.layout_top_toolbar.enable('tb_save')
+    w2ui.layout_top_toolbar.enable('tbDropbox')
+    w2ui.layout_top_toolbar.enable('tb_login')
+  }
+
+
 }
 
 function update_error_status_w2ui(errors) {

commit b378cb7279b4aba6c3819015ac499940d05fe405

#135 fix toggle of save button; bug was introduced in commit 85fbce8

30_sources/SRC_Zupfnoter/public/index.css
@@ -215,6 +215,9 @@ rect.highlight {
     margin-left: 4em;
     list-style: square outside; }
 
+.w2ui-button .alert {
+  background-color: #ff898b; }
+
 #statusbar-layout .w2ui-tb-caption {
   vertical-align: top; }
 

30_sources/SRC_Zupfnoter/public/index.scss
@@ -305,6 +305,10 @@ rect.highlight {
   }
 }
 
+.w2ui-button .alert{
+  background-color: #ff898b;
+}
+
 #statusbar-layout .w2ui-tb-caption {
   vertical-align: top;
 }

30_sources/SRC_Zupfnoter/src/user-interface.js
@@ -805,13 +805,6 @@ function update_systemstatus_w2ui(systemstatus) {
 
   set_tbitem_caption('tb_view', 'Extract ' + systemstatus.view);
 
-  if (systemstatus.music_model == 'changed') {
-    $("#tb_layout_top_toolbar_item_tb_save .w2ui-tb-caption").css("color", "red")
-  } else {
-    $("#tb_layout_top_toolbar_item_tb_save .w2ui-tb-caption").css("color", "")
-  }
-  ;
-
   $(".sb-loglevel").html('Loglevel: ' + systemstatus.loglevel);
   $(".sb-mode").html(w2utils.lang('Mode') + ': ' + systemstatus.mode);
 
@@ -825,10 +818,19 @@ function update_systemstatus_w2ui(systemstatus) {
   else {
     w2ui.layout_top_toolbar.enable('tb_create')
     w2ui.layout_top_toolbar.enable('tb_open')
-    w2ui.layout_top_toolbar.enable('tb_save')
+    // w2ui.layout_top_toolbar.enable('tb_save')
     w2ui.layout_top_toolbar.enable('tbDropbox')
     w2ui.layout_top_toolbar.enable('tb_login')
   }
+
+
+  if (systemstatus.music_model == 'changed') {
+    $("#tb_layout_top_toolbar_item_tb_save table").addClass("alert")
+  } else {
+    $("#tb_layout_top_toolbar_item_tb_save table").removeClass("alert")
+  }
+  ;
+
 
 
 }