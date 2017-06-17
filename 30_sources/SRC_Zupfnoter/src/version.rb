b1096883d79cdef63f8835b6aa6a7aa3f862bf7a
Bernhard Weichel <github.com@nospam.weichel21.de>
2017-06-17 15:39:19 +0200

    publish 1.7.0_dev-1

--- 30_sources/SRC_Zupfnoter/src/version.rb
+++ 30_sources/SRC_Zupfnoter/src/version.rb
@@ -1,3 +1,3 @@
-VERSION = "1.6.1 ++dev"
+VERSION = "1.7.0 dev-1"
 SCHEMA_VERSION = 'https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json'
 COPYRIGHT = "© #{Time.now.year} https://www.zupfnoter.de"
