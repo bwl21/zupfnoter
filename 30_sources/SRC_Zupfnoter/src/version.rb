b923b92c79c16db8823eaf07a156959df1372211
Bernhard Weichel <github.com@nospam.weichel21.de>
2017-11-12 19:27:29 +0100

    Now maintain version number via git describe

--- 30_sources/SRC_Zupfnoter/src/version.rb
+++ 30_sources/SRC_Zupfnoter/src/version.rb
@@ -1,3 +1,3 @@
-VERSION = "1.8.0 dev 2"
+VERSION = "V1.8.0.2-16-g54e55de"
 SCHEMA_VERSION = 'https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json'
 COPYRIGHT = "© #{Time.now.year} https://www.zupfnoter.de"
