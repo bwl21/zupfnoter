e676ca2d60f4c5b16f9c50a6f64af52150891fe3
Bernhard Weichel <github.com@nospam.weichel21.de>
2018-04-07 16:38:26 +0200

    remove opal warning „Freeze not supported“

--- 30_sources/SRC_Zupfnoter/src/confstack2.rb
+++ 30_sources/SRC_Zupfnoter/src/confstack2.rb
@@ -184,7 +184,7 @@ class Hash
   def deep_dup
     hash = dup
     each_pair do |key, value|
-      if key.frozen? && ::String === key
+      if false && ::String === key
         hash[key] = value.deep_dup
       else
         hash.delete(key)
