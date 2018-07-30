bwl21/zupfnoter

commit f669e6b459b057e177ce712530451242d12ab53f

see #222 Suppress jump line if position is 0

30_sources/SRC_Zupfnoter/src/harpnotes.rb
@@ -2007,7 +2007,7 @@ module Harpnotes
 
             unless distance
               old_conf_key = conf_key.gsub(/(.*)\.(\d+)\.(\d+)\.(\w+)/, '\1.\2.\4')
-              distance = show_options[:print_options_raw][old_conf_key]
+              distance     = show_options[:print_options_raw][old_conf_key]
             end
 
           end
@@ -2046,9 +2046,11 @@ module Harpnotes
           path     = Harpnotes::Layout::Default.make_path_from_jumpline(jumpline_info)
           draginfo = {handler: :jumpline, jumpline: jumpline_info, xspacing: $conf.get('layout.X_SPACING')}
 
-          unless goto.policy[:is_repeat] and show_options[:repeatsigns][:voices].include? show_options[:voice_nr]
-            [Harpnotes::Drawing::Path.new(path[0], nil, goto.from).tap { |s| s.conf_key = conf_key; s.conf_value = distance; s.line_width = $conf.get('layout.LINE_THICK'); s.draginfo = draginfo },
-             Harpnotes::Drawing::Path.new(path[1], :filled, goto.from)]
+          unless distance == 0
+            unless goto.policy[:is_repeat] and show_options[:repeatsigns][:voices].include? show_options[:voice_nr]
+              [Harpnotes::Drawing::Path.new(path[0], nil, goto.from).tap { |s| s.conf_key = conf_key; s.conf_value = distance; s.line_width = $conf.get('layout.LINE_THICK'); s.draginfo = draginfo },
+               Harpnotes::Drawing::Path.new(path[1], :filled, goto.from)]
+            end
           end
         end.flatten.compact
 

30_sources/ZGEN_Documents/UD_Zupfnoter-Handbuch-de_review.latex
@@ -204,7 +204,7 @@
 
 \hyphenation{Zupf-noter} \hyphenation{Fen-ster}
 
-Version V\_1.8.0
+Version V\_1.8.0-12-g9735bde
 
 \begin{verbatim}
 © 2018 https://www.zupfnoter.de
@@ -3081,6 +3081,16 @@ Linen rasten beim ziehen mit der Maus zwischen den Saiten ein.
 Konfigurationsparameter ggf. auf mehrere Sprunglinien. Bitte drücke
 daher auf \texttt{Render} um das Endergebnis zu sehen. Danach sind auch
 die Pfeile an den Sprunglinien wieder korrekt.
+\end{quote}
+
+\begin{quote}
+\textbf{Hinweis}: man kann Sprunglinien unterdrücken, indem man im
+Kontext-Menü Kontextmenü \textbf{\texttt{Edit\ config}} wählt, und dann
+den paramteter auf \texttt{0} stellt. Die linie verschwindet dann, und
+kann nur schwer wieder sichtbar gemacht werden:
+\texttt{Konfig.Bearb\ /\ Notenbeezogen}, dann den paramter suchen
+(\texttt{p\_begin}, \texttt{p\_end}, \texttt{p\_follow},
+\texttt{p\_repeat}) und den Wert auf ungleich 0 setzen.
 \end{quote}
 
 \subsubsection{Triolenbogen gestalten}\label{triolenbogen-gestalten}

commit f0de4b034127c01f7165c1435d1db35010a15fe0

see #223 jumplines are no longer surpassed for distance == 1
Bug introduced with commit f669e6b
removed some debuggers

30_sources/SRC_Zupfnoter/src/controller-cli.rb
@@ -149,7 +149,6 @@ class TextPaneEmulatorForCli < Harpnotes::TextPane
 
   def _split_parts(fulltext)
     _clean_models
-`debugger`
     fulltext.split(@config_separator).each_with_index do |part, i|
       if i == 0
         @abc_part = part

30_sources/SRC_Zupfnoter/src/controller.rb
@@ -259,7 +259,6 @@ class Controller
                                current_notes:  [lambda { `update_current_notes_w2ui(#{@harpnote_player.get_notes.join(", ")});` }],
                                settings_menu:  [lambda { `update_settings_menu(#{$settings.to_n})` }],
                                extracts:       [lambda { @extracts.each { |entry|
-                                 `debugger`
                                  title = "#{entry.first}: #{entry.last}"
                                  `set_extract_menu(#{entry.first}, #{title})` }
                                call_consumers(:systemstatus) # restore systemstatus as set_extract_menu redraws the toolbar

30_sources/SRC_Zupfnoter/src/harpnotes.rb
@@ -2023,6 +2023,7 @@ module Harpnotes
 
           distance = goto.policy[:distance] unless distance
           distance = 1 unless distance
+          is_visible = distance == 0 ? false : true;
           distance = distance - 1 if distance > 0 # make distancebeh  symmetric  -1 0 1
 
           from_anchor     = goto.policy[:from_anchor] || :after # after -> reight
@@ -2055,7 +2056,7 @@ module Harpnotes
           path     = Harpnotes::Layout::Default.make_path_from_jumpline(jumpline_info)
           draginfo = {handler: :jumpline, jumpline: jumpline_info, xspacing: $conf.get('layout.X_SPACING')}
 
-          unless distance == 0
+          if is_visible
             unless goto.policy[:is_repeat] and show_options[:repeatsigns][:voices].include? show_options[:voice_nr]
               [Harpnotes::Drawing::Path.new(path[0], nil, goto.from).tap { |s| s.conf_key = conf_key; s.conf_value = distance; s.line_width = $conf.get('layout.LINE_THICK'); s.draginfo = draginfo },
                Harpnotes::Drawing::Path.new(path[1], :filled, goto.from)]