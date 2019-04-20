6ee4c1a9f334247a478b23304331e88a72d41f84
beweiche <git@nospam.weichel21.de>
2019-04-20 16:16:50 +0200

    see #253 set template no also sets the standard notes

--- 30_sources/SRC_Zupfnoter/src/controller_command_definitions.rb
+++ 30_sources/SRC_Zupfnoter/src/controller_command_definitions.rb
@@ -979,8 +979,8 @@ class Controller
       unless template.include?("\nF:{{")
         raise "current file is not a template. It does not have a placeholder in F: line e.g. F:{{song_id}}"
       end
-      handle_command('setstdextract')
       `localStorage.setItem(#{ZN_TEMPLATENAME}, #{template})`
+      handle_command('setstdextract')
     end
     get_current_template # this sets the current template to systemstatus
     nil
@@ -993,6 +993,7 @@ class Controller
     HTTP.get(url, {async: false}) do |response|
       result = response.body
       `localStorage.setItem(#{ZN_TEMPLATENAME}, #{result})`
+      handle_command('setstdextract')
     end
     result
   end
