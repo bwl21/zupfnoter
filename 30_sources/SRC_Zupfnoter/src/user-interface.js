bwl21/zupfnoter

commit f7817a3d0141e9da15aa5675f1d5ee70613a131d

Do not limit number of extracts #269
also refactored userinterface.js

30_sources/SRC_Zupfnoter/src/user-interface.js
@@ -277,24 +277,6 @@ function init_w2ui(uicontroller) {
       toggle_full_screen();
     },
 
-    'tb_view:0': function () {
-      uicontroller.$handle_command("view 0")
-    },
-    'tb_view:1': function () {
-      uicontroller.$handle_command("view 1")
-    },
-    'tb_view:2': function () {
-      uicontroller.$handle_command("view 2")
-    },
-    'tb_view:3': function () {
-      uicontroller.$handle_command("view 3")
-    },
-    'tb_view:4': function () {
-      uicontroller.$handle_command("view 4")
-    },
-    'tb_view:5': function () {
-      uicontroller.$handle_command("view 5")
-    },
     'tbPlay': function () {
       uicontroller.$play_abc('auto');
     },
@@ -326,7 +308,6 @@ function init_w2ui(uicontroller) {
     'tb_logout': function () { // tbDropbox:tb_logout
       uicontroller.$handle_command("dlogout")
     },
-
 
     'tb_login': function () {
       openPopup({
@@ -360,6 +341,39 @@ function init_w2ui(uicontroller) {
           }
         }
       })
+    },
+
+
+    "tb_home": function () {
+      w2popup.open({title: 'About Zupfnoter', body: uicontroller.$about_zupfnoter()})
+    },
+    "tbHelp:tbVersionInfo": function () {
+      window.open(uicontroller.$info_url())
+    },
+    "tbHelp:tbTutorials": function () {
+      window.open("https://www.youtube.com/channel/UCNwzBbzhyHJOn9eHHl_guHg")
+    },
+    "tbHelp:tbAbcTutorial": function () {
+      window.open("http://penzeng.de/Geige/Abc.htm")
+    },
+    "tbHelp:tbAbcTutorialSchacherl": function () {
+      window.open("http://kurs.schacherl.info/ABC-Musiknotation/abc_syntax/abc_syntax.html")
+    },
+    "tbHelp:tbAbcStandard": function () {
+      window.open("http://abcnotation.com/wiki/abc:standard:v2.2")
+    },
+
+    "tbHelp:tbHomepage": function () {
+      window.open("http://www.zupfnoter.de")
+    },
+    "tbHelp:tbManual": function () {
+      window.open("/public/UD_Zupfnoter-Handbuch-de_review.pdf")
+    },
+    "tbHelp:tbReference": function () {
+      window.open("?mode=demo&load=public/demos/3015_reference_sheet.abc")
+    },
+    "tbHelp:tbDemo": function () {
+      window.open("?mode=demo&load=public/demos/zndemo_42_Ich_steh_an_deiner_krippen_hier.abc")
     }
   }
 
@@ -560,13 +574,7 @@ function init_w2ui(uicontroller) {
         icon: 'fa fa-shopping-basket',
         tooltip: "Choose extract",
         items: [
-          {text: 'Extract 0', icon: 'fa fa-asterisk', id: "0"},
-          {},
-          {text: 'Extract 1', icon: 'fa fa-tags', id: "1"},
-          {text: 'Extract 2', icon: 'fa fa-tags', id: "2"},
-          {text: 'Extract 3', icon: 'fa fa-tags', id: "3"},
-          {text: 'Extract 4', icon: 'fa fa-tags', id: "4"},
-          {text: 'Extract 5', icon: 'fa fa-tags', id: "5"}
+          {text: 'Extract 0', icon: 'fa fa-asterisk', id: "0"}
         ]
       },
       {type: 'break'},
@@ -641,9 +649,13 @@ function init_w2ui(uicontroller) {
         previews[event.target]();
       }
 
-      // handle config
+      // handle view
       config_event = event.target.split(":")
-
+      if (config_event[0] = "tb_View"){
+        if (config_event[1]){
+          uicontroller.$handle_command("view " + config_event[1])
+        }
+      }
 
       // this matches the dropbox - menus
       // to the dropbox buttons
@@ -656,36 +668,7 @@ function init_w2ui(uicontroller) {
 
 
       // todo: move these entries to toolbarhandlers
-      if (event.target == "tb_home") {
-        w2popup.open({title: 'About Zupfnoter', body: uicontroller.$about_zupfnoter()})
-      }
-      if (event.target == "tbHelp:tbVersionInfo") {
-        window.open(uicontroller.$info_url())
-      }
-      if (event.target == "tbHelp:tbTutorials") {
-        window.open("https://www.youtube.com/channel/UCNwzBbzhyHJOn9eHHl_guHg")
-      }
-      if (event.target == "tbHelp:tbAbcTutorial") {
-        window.open("http://penzeng.de/Geige/Abc.htm")
-      }
-      if (event.target == "tbHelp:tbAbcTutorialSchacherl") {
-        window.open("http://kurs.schacherl.info/ABC-Musiknotation/abc_syntax/abc_syntax.html")
-      }
-      if (event.target == "tbHelp:tbAbcStandard") {
-        window.open("http://abcnotation.com/wiki/abc:standard:v2.2")
-      }
-      if (event.target == "tbHelp:tbHomepage") {
-        window.open("http://www.zupfnoter.de")
-      }
-      if (event.target == "tbHelp:tbManual") {
-        window.open("/public/UD_Zupfnoter-Handbuch-de_review.pdf")
-      }
-      if (event.target == "tbHelp:tbReference") {
-        window.open("?mode=demo&load=public/demos/3015_reference_sheet.abc")
-      }
-      if (event.target == "tbHelp:tbDemo") {
-        window.open("?mode=demo&load=public/demos/zndemo_42_Ich_steh_an_deiner_krippen_hier.abc")
-      }
+
       if (event.subItem && event.subItem.onClick) {
         event.subItem.onClick();
       }
@@ -1353,7 +1336,12 @@ function before_open() {
   w2ui.layout_left_tabs.click('abceditortab')
 };
 
-function set_extract_menu(id, text) {
+function set_extract_menu(items){
+  debugger;
+  w2ui.layout_top_toolbar.set('tb_view', {items: items})
+}
+
+function set_extract_menua(id, text) {
   w2ui.layout_top_toolbar.set('tb_view:' + id, {text: text});
   // w2ui.layout_top_toolbar.refresh();  we do not need this, in the reas application as we subsequentlcy  redraw the toolbar
   // by call_consumers(:extracts)

30_sources/SRC_Zupfnoter/src/abc2svg_to_harpnotes.rb
@@ -661,7 +661,6 @@ module Harpnotes
         unless @next_note_marks[:measure]
           start_pos = charpos_to_line_column(voice_element[:istart])
           end_pos   = charpos_to_line_column(voice_element[:iend])
-          JS.debugger
           $log.warning("abc:#{start_pos.first}:#{start_pos.last} Error: " + I18n.t("Meter change not at beginning of measure"), start_pos, end_pos)
         end
 

30_sources/SRC_Zupfnoter/src/controller.rb
@@ -284,10 +284,10 @@ class Controller
                                document_title:    [lambda { `document.title = #{@document_title}` }],
                                current_notes:     [lambda { `update_current_notes_w2ui(#{@harpnote_player.get_notes.join(", ")});` }],
                                settings_menu:     [lambda { `update_settings_menu(#{$settings.to_n})` }],
-                               extracts:          [lambda { @extracts.each { |entry|
-                                 title = "#{entry.first}: #{entry.last}"
-                                 `set_extract_menu(#{entry.first}, #{title})` }
-                               call_consumers(:systemstatus) # restore systemstatus as set_extract_menu redraws the toolbar
+                               extracts: [lambda {
+                                 items = @extracts.map { |id, entry| {id: id, text: "#{id}: #{entry}"} }
+                                 `set_extract_menu(#{items.to_n})`
+                                 call_consumers(:systemstatus) # restore systemstatus as set_extract_menu redraws the toolbar
                                }],
                                harp_preview_size: [lambda { %x{set_harp_preview_size(#{@harp_preview_size})} }],
                                render_status:     [lambda { %x{set_render_status(#{@systemstatus[:autorefresh]}+ ' '+ #{@render_stack.to_s})} }],

30_sources/SRC_Zupfnoter/src/controller_command_definitions.rb
@@ -380,7 +380,6 @@ class Controller
                             .gsub(/^T:.*$/, "T:{{song_title}}")
         @editor.set_text(template)
         @editor.delete_config_part('template')
-        JS.debugger
         handle_command("editconf template")
         nil
       end
@@ -471,6 +470,7 @@ class Controller
             'subflowlines'     => lambda { {key: "extract.#{@systemstatus[:view]}.subflowlines", value: $conf['extract.0.subflowlines']} },
             'produce'          => lambda { {key: "produce", value: $conf['produce']} },
             'annotations'      => lambda { {key: "annotations.x", value: $conf['templates.annotations']} },
+            'extracts'         => lambda { {key: "extract.x", value: $conf['templates.extracts']} },
             'layout'           => lambda { {key:   "extract.#{@systemstatus[:view]}.layout",
                                             value: $conf['extract.0.layout']} }, # radii of the largest Rest Glyph} },
             'printer' => lambda { {key:   "extract.#{@systemstatus[:view]}.printer",
@@ -631,7 +631,9 @@ class Controller
       # the inital array specifies the available
       # extracts
       def expand_extractnumbering(keys)
-        [0, 1, 2, 3, 4, 5].product(keys).map { |number, key| "extract.#{number}.#{key}" }
+        extract_from_editor = @editor.get_config_model.first[:extract] || {}
+        extracts = (extract_from_editor.keys + $conf[:extract].keys).map{|i|i.to_i}.uniq.sort
+        extracts.product(keys).map { |number, key| "extract.#{number}.#{key}" }
       end
 
       command.undoable = false
@@ -644,7 +646,6 @@ class Controller
 
       command.as_action do |args|
         $log.timestamp("editconf #{args[:set]}  #{__FILE__} #{__LINE__}")
-
         # this is the set of predefined configuration pages
         # it is the argument of editconf {set}
         #
@@ -659,10 +660,9 @@ class Controller
                                                                             'stringnames.vpos', 'sortmark.show',
                                                                            ]) + [:restposition]},
             extract_annotation:    {keys:                  [:produce,
-                                                            'extract.0.notes.T05_printed_extracts.text',
-                                                            'extract.0.notes.T01_number.text',
-                                                            expand_extractnumbering(['title', 'filenamepart', 'notes.T01_number_extract.text'])].flatten,
+                                                            expand_extractnumbering(['title', 'voices', 'filenamepart'])].flatten,
                                     quicksetting_commands: ['stdextract'],
+                                    newentry_handler:      lambda { handle_command("addconf extracts")},
                                     scope:                 :global
             },
             barnumbers_countnotes: {keys:                  expand_extract_keys(['barnumbers.voices', 'barnumbers.pos', 'barnumbers.autopos', 'barnumbers.apanchor', 'barnumbers.apbase', 'barnumbers.style',
@@ -748,7 +748,6 @@ class Controller
             end
           end
         end
-
         # see if we have a search form set ---------------------------------------------------
         unless the_form
           econf = Confstack.new

30_sources/SRC_Zupfnoter/src/init_conf.rb
@@ -76,7 +76,8 @@ module InitConf
              lyrics:      {verses: [1], pos: [350, 70], style: :regular},
              images:      {imagename: "", show: true, pos: [10, 10], height: 100, },
              tuplet:      {cp1: [5, 2], cp2: [5, -2], shape: ['c'], show: true},
-             annotations: {text: "_vorlage_", pos: [-5, -6]} # Notenbeschriftungsvorlage
+             annotations: {text: "_vorlage_", pos: [-5, -6]}, # Notenbeschriftungsvorlage
+             extracts:    {title: "", filenamepart: "-", notes:{T01_number_extract:{text: "{{extract_filename}}"}}}
          },
 
          # this is used to populate a QuickSettings menu
@@ -470,25 +471,6 @@ module InitConf
                      a4_pages:    [0, 1, 2],
                      show_border: false
                  }
-             },
-             "1" => {
-                 title:  "Sopran, Alt",
-                 voices: [1, 2]
-             },
-             "2" => {
-                 title:  "Tenor, Bass",
-                 voices: [3, 4]
-             },
-             "3" => {
-                 title:  "Melodie",
-                 voices: [1]
-             },
-             "4" => {
-                 title:  "Extract 4",
-                 voices: [1]
-             },
-             "5" => {
-                 title: "Extract 5", voices: [1]
              }
          },
 

30_sources/SRC_Zupfnoter/src/text_pane.rb
@@ -445,6 +445,10 @@ module Harpnotes
       @config_models[:config]
     end
 
+    # get the config model
+    # return arry of [config_model, status]
+    # status indicates if we got a model from editor
+    # todo: cleant this up
     def get_config_model
       config_model = _get_config_model
       config_model ? [_get_config_model, true] : [{}, false]

commit dd5939032663dcaba67011b3bc7618e7230eb4d4

Fix edit flowconf
broken since f7817a3d0141e9da15aa5675f1d5ee70613a131d

30_sources/SRC_Zupfnoter/src/user-interface.js
@@ -651,7 +651,7 @@ function init_w2ui(uicontroller) {
 
       // handle view
       config_event = event.target.split(":")
-      if (config_event[0] = "tb_View"){
+      if (config_event[0] == "tb_View"){
         if (config_event[1]){
           uicontroller.$handle_command("view " + config_event[1])
         }