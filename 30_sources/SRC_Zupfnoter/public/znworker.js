bwl21/zupfnoter

commit 30d315ef233b351aca0268f94ca9453d3e2289b9

prepare 1.14

30_sources/SRC_Zupfnoter/public/znworker.js
@@ -40287,7 +40287,7 @@ Opal.modules["version-prod"] = function(Opal) {
 
   Opal.add_stubs(['$year', '$now']);
   
-  Opal.const_set($nesting[0], 'VERSION', "V_1.13-7-g9cb82600");
+  Opal.const_set($nesting[0], 'VERSION', "V_1.13-10-g32ab2730");
   Opal.const_set($nesting[0], 'SCHEMA_VERSION', "https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json");
   return Opal.const_set($nesting[0], 'COPYRIGHT', "" + "© " + ($$($nesting, 'Time').$now().$year()) + " https://www.zupfnoter.de");
 };

30_sources/SRC_Zupfnoter/src/application-cli.rb
@@ -136,6 +136,7 @@ sourcefiles.each do |sourcefile|
     %x{
        var encoding = require ("encoding")
        var buffer = encoding.convert(#{content}, 'Latin-1')
+       var buffer = #{content};
        fs.writeFileSync(#{outputname}, buffer)
     }
     nil

commit 26d934e6f619565bbce1dcb2d4808277061a3eda

see #292 fix zupfnoter_cli broken by 30d315e

200_zupfnoter.iml
@@ -9,8 +9,11 @@
     <orderEntry type="library" scope="PROVIDED" name="bundler (v2.1.2, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="coderay (v1.1.2, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="concurrent-ruby (v1.1.5, RVM: ruby-2.6.3) [gem]" level="application" />
+    <orderEntry type="library" scope="PROVIDED" name="diff-lcs (v1.3, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="ffi (v1.11.3, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="graphviz (v1.1.0, RVM: ruby-2.6.3) [gem]" level="application" />
+    <orderEntry type="library" scope="PROVIDED" name="i18n (v1.7.0, RVM: ruby-2.6.3) [gem]" level="application" />
+    <orderEntry type="library" scope="PROVIDED" name="json (v2.2.0, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="json-schema-generator (v0.0.9, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="jsonpath (v1.0.4, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="method_source (v0.9.2, RVM: ruby-2.6.3) [gem]" level="application" />
@@ -29,6 +32,12 @@
     <orderEntry type="library" scope="PROVIDED" name="rb-inotify (v0.10.1, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="rb-readline (v0.5.5, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="redcarpet (v3.4.0, RVM: ruby-2.6.3) [gem]" level="application" />
+    <orderEntry type="library" scope="PROVIDED" name="rspec (v3.9.0, RVM: ruby-2.6.3) [gem]" level="application" />
+    <orderEntry type="library" scope="PROVIDED" name="rspec-core (v3.9.0, RVM: ruby-2.6.3) [gem]" level="application" />
+    <orderEntry type="library" scope="PROVIDED" name="rspec-expectations (v3.9.0, RVM: ruby-2.6.3) [gem]" level="application" />
+    <orderEntry type="library" scope="PROVIDED" name="rspec-mocks (v3.9.0, RVM: ruby-2.6.3) [gem]" level="application" />
+    <orderEntry type="library" scope="PROVIDED" name="rspec-support (v3.9.0, RVM: ruby-2.6.3) [gem]" level="application" />
+    <orderEntry type="library" scope="PROVIDED" name="rubyzip (v2.0.0, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="sass (v3.7.4, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="sass-listen (v4.0.0, RVM: ruby-2.6.3) [gem]" level="application" />
     <orderEntry type="library" scope="PROVIDED" name="sprockets (v3.7.2, RVM: ruby-2.6.3) [gem]" level="application" />

30_sources/SRC_Zupfnoter/public/znworker.js
@@ -40305,7 +40305,7 @@ Opal.modules["version-prod"] = function(Opal) {
 
   Opal.add_stubs(['$year', '$now']);
   
-  Opal.const_set($nesting[0], 'VERSION', "V_1.14-3-g34056b75");
+  Opal.const_set($nesting[0], 'VERSION', "V_1.14-4-gbc7cfa3e");
   Opal.const_set($nesting[0], 'SCHEMA_VERSION', "https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json");
   return Opal.const_set($nesting[0], 'COPYRIGHT', "" + "© " + ($$($nesting, 'Time').$now().$year()) + " https://www.zupfnoter.de");
 };

30_sources/SRC_Zupfnoter/src/application-cli.rb
@@ -136,7 +136,6 @@ sourcefiles.each do |sourcefile|
     %x{
        var encoding = require ("encoding")
        var buffer = encoding.convert(#{content}, 'Latin-1')
-       var buffer = #{content};
        fs.writeFileSync(#{outputname}, buffer)
     }
     nil

30_sources/SRC_Zupfnoter/src/zupfnoter-cli.js
@@ -54676,7 +54676,7 @@ Opal.modules["version-prod"] = function(Opal) {
 
   Opal.add_stubs(['$year', '$now']);
   
-  Opal.const_set($nesting[0], 'VERSION', "V_1.14-3-g34056b75");
+  Opal.const_set($nesting[0], 'VERSION', "V_1.14-4-gbc7cfa3e");
   Opal.const_set($nesting[0], 'SCHEMA_VERSION', "https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json");
   return Opal.const_set($nesting[0], 'COPYRIGHT', "" + "© " + ($$($nesting, 'Time').$now().$year()) + " https://www.zupfnoter.de");
 };
@@ -81202,7 +81202,7 @@ Opal.loaded(["./abc2svg-1.js"]);
       
        var encoding = require ("encoding")
        var buffer = encoding.convert(content, 'Latin-1')
-       var buffer = content;
+       //var buffer = content;
        fs.writeFileSync(outputname, buffer)
     ;
       return nil;}, $$2.$$s = self, $$2.$$arity = 2, $$2));