c4c9ebb0fa5c54e8fdf62e877c0a3a87df65e3a9
Bernhard Weichel <github.com@nospam.weichel21.de>
2020-02-27 13:47:22 +0100

    recompiled

--- 30_sources/SRC_Zupfnoter/public/znworker.js
+++ 30_sources/SRC_Zupfnoter/public/znworker.js
@@ -40349,7 +40349,7 @@ Opal.modules["version-prod"] = function(Opal) {
 
   Opal.add_stubs(['$year', '$now']);
   
-  Opal.const_set($nesting[0], 'VERSION', "V_1.14-22-g38fd4ae6");
+  Opal.const_set($nesting[0], 'VERSION', "V_1.14-22-ga752497f");
   Opal.const_set($nesting[0], 'SCHEMA_VERSION', "https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json");
   return Opal.const_set($nesting[0], 'COPYRIGHT', "" + "© " + ($$($nesting, 'Time').$now().$year()) + " https://www.zupfnoter.de");
 };
