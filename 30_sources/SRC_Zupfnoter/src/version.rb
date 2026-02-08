d84db0f9c454d645e98792e1d430e43576586622
Bernhard Weichel <bernhard.weichel@t-online.de>
2026-02-08 21:12:15 +0100

    commit build results

--- 30_sources/SRC_Zupfnoter/src/version.rb
+++ 30_sources/SRC_Zupfnoter/src/version.rb
@@ -1,3 +1,3 @@
-VERSION = "git describe output here"
+VERSION = "1.17"
 SCHEMA_VERSION = 'https://zupfnoter.weichel21.de/schema/zupfnoter-config_1.0.json'
 COPYRIGHT = "© #{Time.now.year} https://www.zupfnoter.de"
